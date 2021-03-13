6036a52a06bb4111c4afe097918a2794bf4cd12d
SkyorYT <64802906+SkyorYT@users.noreply.github.com>
2021-03-13 15:26:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qev4ajyjdwvcslue39rljed854lcvw34p8h0pcm.test1
 
 
 done
